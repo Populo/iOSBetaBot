fb0154083bf2b013596af98c148ba72e46bd0011
Populo <pxpulo@gmail.com>
2023-07-06 22:36:35 -0400

    Add project files.

---
+++ iOSBetaBot/IosBot.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Rest;
+
+namespace iOSBetaBot
+{
+    internal class IosBot
+    {
+        private DiscordRestClient _client { get; set; }
+
+        static void Main(string[] args) => new IosBot().MainAsync(args);
+
+        private void MainAsync(string[] args)
+        {
+            if (args.Length == 0) { throw new Exception("Provide Token"); }
+
+            _client = new DiscordRestClient();
+
+            _client.LoginAsync(Discord.TokenType.Bot, args[0]);
+        }
+    }
+}
---
+++ iOSBot.Data/BetaContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+
+namespace iOSBot.Data
+{
+    public class BetaContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Server> Servers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) {
+                var connection = new MySqlConnectionStringBuilder();
+                connection.Server = "pinas";
+                connection.UserID = "BetaBot";
+                connection.Password = "1UwDO;FHwz{*Z+@IJY4a";
+
+#if DEBUG
+                connection.Database = "BetaBotDev";
+#else
+                connection.Database = "BetaBot";
+#endif
+
+                optionsBuilder.UseMySql(connection.ConnectionString, ServerVersion.AutoDetect(connection.ConnectionString));
+            }
+        }
+    }
+
+    public class Post
+    {
+        public string Guid { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+
+    public class Server
+    {
+        public ulong ServerId { get; set; }
+        public ulong ChannelId { get; set; }
+    }
+}
